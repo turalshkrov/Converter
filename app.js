28a10ecba68bc08d3b00a1029ead2ccf41c63aae
Tural Şükürov <tural.shkrov@gmail.com>
2022-04-23 21:12:18 +0400

    js

---
+++ app.js
@@ -0,0 +1,51 @@
+let leftBtn = document.querySelector('#left-btn');
+let rightBtn = document.querySelector('#right-btn');
+let leftInput = document.querySelector('#l-input');
+let rightInput = document.querySelector('#r-input');
+let url = 'https://api.exchangerate.host/latest';
+let cur1 = 'RUB';
+let cur2 = 'USD';
+let rate;
+
+getRates('RUB', 'USD');
+
+leftBtn.addEventListener('click', async e => {
+    btnColorChange(e);
+    cur1 = e.target.innerText;
+    await getRates(cur1, cur2);
+    rightInput.value = leftInput.value * rate;
+})
+rightBtn.addEventListener('click', async e => {
+    btnColorChange(e);
+    cur2 = e.target.innerText;
+    await getRates(cur1, cur2);
+    leftInput.value = rightInput.value * rate;
+})
+leftInput.addEventListener('keyup', e => {
+    convert(e);
+})
+rightInput.addEventListener('keyup', e => {
+    convert(e);
+})
+
+function convert(e) {
+    if (e.target.id == 'l-input') {
+        rightInput.value = e.target.value * rate;
+    }
+    else {
+        leftInput.value = e.target.value * Math.round(1 / rate * 10000) / 10000;
+    }
+}
+
+function btnColorChange(e) {
+    for (let i = 0; i < 4; i++) {
+        e.target.parentElement.children[i].className = 'cur-btn';
+    }
+    e.target.className = 'cur-btn cur-btn-active';
+}
+
+async function getRates(cur1, cur2) {
+    const data = await fetch(`${url}?base=${cur1}&symbols=${cur2}`);
+    const res = await data.json();
+    rate = Math.round(res.rates[cur2] * 10000) / 10000;
+}
